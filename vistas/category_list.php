c63cbc4724024cfa06a5e2421187a70a7a347e2d
DanielSmitarello <dsmitarello@gmail.com>
2024-04-10 18:44:38 -0300

    Y ahora si...?

--- vistas/category_list.php
+++ vistas/category_list.php
@@ -9,7 +9,7 @@
 
     require_once "./php/main.php";
 
-    # Eliminar categoría #
+    # Eliminar usuario #
     if (isset($_GET['category_id_del'])) {
         require_once "./php/categoria_eliminar.php";
     }
@@ -28,7 +28,7 @@
     $registros = 15;
     $busqueda = "";
 
-    # Paginador categoría #
+    # Paginador usuario #
     require_once "./php/categoria_lista.php";
 
     ?>
